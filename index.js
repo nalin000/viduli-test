e311ba78ec210947e6467c96bf90cf71f2fb063f
nalin000 <nalintrivedi000@gmail.com>
2025-12-22 13:13:39 +0530

    simple health api

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/health") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ status: "ok" }));
+  }
+});
+
+server.listen(process.env.PORT || 3000);
